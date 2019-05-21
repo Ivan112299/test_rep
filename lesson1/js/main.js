ccac6d190a9e2bf9c7d48f43cd4b1c2e3eafcb9d
Ivan_Kozin <kozinvanya@mail.ru>
2019-05-21 19:02:48 +0300

    первый комит

---
+++ lesson1/js/main.js
@@ -0,0 +1,35 @@
+var coins = prompt('Ваш бюджет?'); //вопрос и ответ в премунную эту
+var nameShop = prompt('Название вашего магазина?'); //вопрос и ответ в премунную эту
+
+	var quest1 = prompt('Какой тип товаров будем продавать?'); //вопрос и ответ в премунную эту
+	var quest2 = prompt('Какой тип товаров будем продавать?');//вопрос и ответ в премунную эту
+	var quest3 = prompt('Какой тип товаров будем продавать?');//вопрос и ответ в премунную эту
+
+var shopGoods = [quest1, quest2, quest3] //закинул в архив переменные
+
+console.log(shopGoods[2]) // проверил что переменные в архиве, именно переменные
+console.log(coins); //просо вывел знчение в консоль
+console.log(nameShop);//просо вывел знчение в консоль
+
+alert(coins/30) //показал алерт где делю переменную на 30
+
+var arrOffers = ['cat', 'dog', 'pig']; //создал архив
+var employers = {}; //создал и наполнил объект  - obj
+
+employers = {
+	kass: 'Ivan',
+	security: 'Jhon',
+	director: 'DonOmar'
+};
+
+var mainList = {}; //создал и наполнил объект - obj
+
+mainList = {
+	coins: coins,
+	nameShop: nameShop,
+	arrOffers: arrOffers,
+	employers: employers,
+	open: true
+};
+
+console.log(mainList['arrOffers']); //вывел массив в консоль
---
+++ script.js
@@ -0,0 +1,54 @@
+var first = 1;
+const pi = 3.14;
+
+var number = 5;
+var string = "hello";
+null; // отдельный тип данных
+undefined; // объявлена но незначена
+var obj = {}
+
+{
+	let second = 2;
+
+}
+
+obj = {
+	name: "Ivan",
+	age: 25,
+	isMarried: false
+}
+
+console.log(obj['name']);
+
+var arr = ['plum', 'orange','apple'];
+
+//console.log(arr[1]);
+//confirm('seriosly?');
+//var answer = prompt('Вам есть 18?','18');
+//var answer = +prompt('Вам есть 18?','18'); //тут пставили + и превратили в number
+
+console.log( typeof(answer)); //выводим како йтип данных значение в скобках
+
+var incr = 10;
+	decr = 10;
+
+	incr++;
+	decr--;
+console.log(incr)
+
+console.log(5%2); //выводит остаток от деления
+// = присваиванрие
+// == сравнение
+// === сравнение типов данных
+
+var isCheked = true;
+var isClose = true;
+
+console.log(isClose && !isCheked) //&& - логическое или
+console.log(isClose || !isCheked)  // || - логическое или
+
+
+
+
+
+
